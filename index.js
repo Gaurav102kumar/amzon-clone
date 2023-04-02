364404a0efb34732fab0b8d4d73c334ef8e0b64d
Gaurav102kumar <92779299+Gaurav102kumar@users.noreply.github.com>
2023-04-01 22:54:42 -0700

    change

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const productRoute=require('./routes/products')
 const orderRouter=require('./routes/cart')
 
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+
 
 
 
